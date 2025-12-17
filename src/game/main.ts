bd2cee70d2b4de852333d23d27d0a76fb1d8e4dd
Daniel Mikusa <dan@mikusa.com>
2025-12-16 23:57:31 -0500

    Add high score keeping
    Signed-off-by: Daniel Mikusa <dan@mikusa.com>

--- src/game/main.ts
+++ src/game/main.ts
@@ -2,6 +2,7 @@ import { Boot } from './scenes/Boot';
 import { GameOver } from './scenes/GameOver';
 import { Game as MainGame } from './scenes/Game';
 import { GameScene } from './scenes/GameScene';
+import { HighScores } from './scenes/HighScores';
 import { MainMenu } from './scenes/MainMenu';
 import { AUTO, Game } from 'phaser';
 import { Preloader } from './scenes/Preloader';
@@ -14,7 +15,7 @@ const config: Phaser.Types.Core.GameConfig = {
   height: 768,
   parent: 'game-container',
   backgroundColor: '#028af8',
-  scene: [Boot, Preloader, MainMenu, MainGame, GameOver, GameScene],
+  scene: [Boot, Preloader, MainMenu, HighScores, MainGame, GameOver, GameScene],
 };
 
 const StartGame = (parent: string) => {
